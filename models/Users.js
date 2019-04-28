2c0368265de53c6fd806f8d14e5755c16be8947b
suprim <king_suprim@yahoo.com>
2019-04-28 17:29:32 +0545

    validation setup using joi and bcrypt for login and registration

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 // Schema
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -13,5 +14,15 @@ const userSchema = new mongoose.Schema({
 });
 // Model
 const Users = new mongoose.model('users', userSchema);
-
+// Validation
+function userValidation(user) {
+    const schema = {
+        name: Joi.string().min(3).max(30).required(),
+        email: Joi.string().email().min(3).max(30).required(),
+        password: Joi.string().min(3).required(),
+        avatar: Joi.string(),
+    }
+    return Joi.validate(user, schema);
+}
 exports.Users = Users;
+exports.Validate = userValidation;
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,19 +4,23 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 // Models
-const { Users } = require('../../models/Users');
+const { Users, Validate } = require('../../models/Users');
+
 
 router.get('/', (req, res) => {
     res.send('users')
 });
-
+// Register
 router.post('/register', (req, res) => {
+    // Validation
+    const { error } = Validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     // Check Email
     Users.findOne({ email: req.body.email }).then(user => {
         if (user) {
             return res.status(400).json({ email: 'Email is already Registerd.' });
         } else {
-            // Avatart
+            // Avatar
             const avatar = gravatar.url(req.body.email, { s: '200', r: 'pg', d: 'mm' })
             // Create
             const newuser = new Users({
@@ -32,9 +36,31 @@ router.post('/register', (req, res) => {
                     newuser.password = hash;
                     // save
                     newuser.save().then(user => res.json(user)).catch(err => console.error(err));
-                })
+                });
             });
         }
+    })
+});
+// Login
+router.get('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Check Email Exist
+    Users.findOne({ email: email }).then(user => {
+        if (!user) {
+            return res.status(404).json({ email: 'Email is not registred.' });
+        }
+        // Check Password is Correct
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // Generate Payload
+                // Generate Token
+                res.json({ msg: "Login Success" });
+            } else {
+                return res.status(400).json({ password: 'Password is incorrect' });
+            }
+        })
     })
 });
 module.exports = router;
